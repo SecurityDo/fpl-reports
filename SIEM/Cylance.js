bb7514c2678530a2e509958d6942b29e5015fad5
Kun@Stream <kun@fluencysecurity.com>
2023-07-29 14:13:21 -0400

    add Cylance threat report

---
+++ SIEM/Cylance.js
@@ -0,0 +1,20 @@
+function main() {
+  return pluginLambda("Cylance",   "pagoNetworksHq", (customer) => {      
+       let threats = Cylance_LoadThreat({start_time: "-7d"}, (obj) => {
+          let {sha256, md5, name, global_quarantined,safelisted,  classification, sub_classification} = obj
+          let ID = uuid()
+              return {ID, sha256, name, global_quarantined, classification, safelisted, sub_classification, customer}
+          })
+        threats.NewColumns( ({sha256}) => {
+           let devices = Cylance_GetThreatDevices(sha256)
+           let filePaths = []
+           for (let i = 0; i < len(devices); i++) {
+             let device = devices[i]
+             filePaths = append(filePaths, device.name + device.file_path) 
+           }
+           let info = Cylance_GetThreatInfo(sha256) 
+           return {filePaths, detected_by: info.detected_by}
+        })  
+        return {threats}
+  })
+}
