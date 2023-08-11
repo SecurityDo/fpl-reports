c9918f876affe4c1c6a8e3ed1c0e778d470704c2
Kun@Stream <kun@fluencysecurity.com>
2023-08-11 16:45:42 -0400

    update AD resource load

--- SIEM/loadResource_AD.js
+++ SIEM/loadResource_AD.js
@@ -1,16 +1,17 @@
 function main() {
 
-  let assets = Fluency_ResourceLoad("AD", "asset", "*", (obj) => {
+  let assets = Fluency_ResourceLoad("AD", "asset", "*", (obj, customer) => {
       let fields = obj["@ADAsset"]
       let {customer, name, sAMAccountName, description, dNSHostName, updatedOn, propertyFlags} = fields    
       // return {name, sAMAccountName, description, dNSHostName, updatedOn, propertyFlags} 
-         
+      if propertyFlags.Some((_, e) => e == "ACCOUNTDISABLE") {
+        return null
+      }   
       return {
          aggregate:{
            groupBy:{name},
            columns: {
-             argmax:{updatedOn, sAMAccountName, description, dNSHostName, propertyFlags}, 
-             values:{customers:customer}
+             argmax:{updatedOn, sAMAccountName, description, dNSHostName, propertyFlags, customer} 
          }
        }       
   })
