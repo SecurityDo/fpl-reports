44e5cf6b176abb34a8b83c1fe8f91c8990c233bb
Kun@Stream <kun@fluencysecurity.com>
2023-08-12 18:08:28 -0400

    update doc for Join/Append

---
+++ SIEM/loadResource_office365Device.js
@@ -0,0 +1,26 @@
+function main() {
+
+  let assets = Fluency_ResourceLoad("Office365", "device", "*", (obj, customer) => {
+      let fields = obj["@office365Device"]
+      let {displayName: name, registrationDateTime:updatedOn, enrollmentProfileName, approximateLastSignInDateTime, accountEnabled, operatingSystem, enrollmentType} = fields
+      
+      if !accountEnabled {
+        return null
+      }
+            
+      // return {name, registrationDateTime, enrollmentProfileName, approximateLastSignInDateTime, accountEnabled, operatingSystem, enrollmentType}          
+      let normalize = tolower(name)
+      
+      return {
+         aggregate:{
+           groupBy:{normalize},
+           columns: {
+             argmax:{updatedOn},
+             values:{customers: enrollmentProfileName}
+           }
+         }
+      }          
+  })
+  return {assets}
+}
+
