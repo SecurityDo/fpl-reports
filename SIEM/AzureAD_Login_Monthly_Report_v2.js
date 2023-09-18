ff8e886b83fc6031a7bef5e21a010f4969b3a3ce
Yuan Shen Tay <yuanshentay@gmail.com>
2023-09-18 17:27:33 -0400

    fix dates and improved azureAD monthly report

--- SIEM/AzureAD_Login_Monthly_Report_v2.js
+++ SIEM/AzureAD_Login_Monthly_Report_v2.js
@@ -75,74 +75,50 @@ function fetchAzureSignInByCity(from, to) {
     return table
 }
 
-function main() {
-    let usedSignApp1=fetchAzureSignInByAppDisplayName("-30d<d","-25d>d")
-    let usedSignApp2=fetchAzureSignInByAppDisplayName("-25d<d","-20d>d")
-    let usedSignApp3=fetchAzureSignInByAppDisplayName("-20d<d","-15d>d")
-    let usedSignApp4=fetchAzureSignInByAppDisplayName("-15d<d","-10d>d")
-    let usedSignApp5=fetchAzureSignInByAppDisplayName("-10d<d","-5d>d")
-    let usedSignApp6=fetchAzureSignInByAppDisplayName("-5d<d","-1d>d")
-    let usedSignApp = mergeTable(usedSignApp1, usedSignApp2, usedSignApp3, usedSignApp4, usedSignApp5, usedSignApp6).Aggregate(({appDisplayName, total})=>{
+function main({from = "-30d<d", to = "@d", interval= "5d"}) {
+    let rangeFrom = new Time(from)
+    let rangeTo = new Time(to)
+    let usedSignApp = new Table()
+    let userFreq = new Table()
+    let usedOS = new Table()
+    let userBrowser = new Table()
+    let signInCountry = new Table()
+    let signInCity = new Table()
+    let userEmailFreq = new Table()
+    for let t = rangeFrom; t.Before(rangeTo); t = t.Add(interval) {
+        usedSignApp.Append(fetchAzureSignInByAppDisplayName(t, t.Add(interval)))
+        userFreq.Append(fetchAzureSignInByUserDisplayName(t, t.Add(interval)))
+        usedOS.Append(fetchAzureSignInByOperatingSystem(t, t.Add(interval)))
+        userBrowser.Append(fetchAzureSignInByBrowser(t, t.Add(interval)))
+        signInCountry.Append(fetchAzureSignInByCountryOrRegion(t, t.Add(interval)))
+        signInCity.Append(fetchAzureSignInByCity(t, t.Add(interval)))
+        userEmailFreq.Append(fetchAzureSignInByUserPrincipalName(t, t.Add(interval)))
+    }
+    usedSignApp = usedSignApp.Aggregate(({appDisplayName, total})=>{
         return {groupBy: {appDisplayName}, columns: {sum: {total}}}
     }).Sort(25, "total")
         
-    let userFreq1 = fetchAzureSignInByUserDisplayName("-30d<d","-25d>d")
-    let userFreq2 = fetchAzureSignInByUserDisplayName("-25d<d","-20d>d")
-    let userFreq3 = fetchAzureSignInByUserDisplayName("-20d<d","-15d>d")
-    let userFreq4 = fetchAzureSignInByUserDisplayName("-15d<d","-10d>d")
-    let userFreq5 = fetchAzureSignInByUserDisplayName("-10d<d","-5d>d")
-    let userFreq6 = fetchAzureSignInByUserDisplayName("-5d<d","-1d>d")
-    let userFreq = mergeTable(userFreq1, userFreq2, userFreq3, userFreq4, userFreq5, userFreq6).Aggregate(({userDisplayName, total})=>{
+    userFreq = userFreq.Aggregate(({userDisplayName, total})=>{
         return {groupBy: {userDisplayName}, columns: {sum: {total}}}
     }).Sort(10, "total")
 
-    let usedOS1 = fetchAzureSignInByOperatingSystem("-30d<d","-25d>d")
-    let usedOS2 = fetchAzureSignInByOperatingSystem("-25d<d","-20d>d")
-    let usedOS3 = fetchAzureSignInByOperatingSystem("-20d<d","-15d>d")
-    let usedOS4 = fetchAzureSignInByOperatingSystem("-15d<d","-10d>d")
-    let usedOS5 = fetchAzureSignInByOperatingSystem("-10d<d","-5d>d")
-    let usedOS6 = fetchAzureSignInByOperatingSystem("-5d<d","-1d>d")
-    let usedOS = mergeTable(usedOS1, usedOS2, usedOS3, usedOS4, usedOS5, usedOS6).Aggregate(({operatingSystem, total})=>{
+    usedOS = usedOS.Aggregate(({operatingSystem, total})=>{
         return {groupBy: {operatingSystem}, columns: {sum: {total}}}
     }).Sort(25, "total")
 
-    let userBrowser1 = fetchAzureSignInByBrowser("-30d<d","-25d>d")
-    let userBrowser2 = fetchAzureSignInByBrowser("-25d<d","-20d>d")
-    let userBrowser3 = fetchAzureSignInByBrowser("-20d<d","-15d>d")
-    let userBrowser4 = fetchAzureSignInByBrowser("-15d<d","-10d>d")
-    let userBrowser5 = fetchAzureSignInByBrowser("-10d<d","-5d>d")
-    let userBrowser6 = fetchAzureSignInByBrowser("-5d<d","-1d>d")
-    let userBrowser = mergeTable(userBrowser1, userBrowser2, userBrowser3, userBrowser4, userBrowser5, userBrowser6).Aggregate(({browser, total})=>{
+    userBrowser = userBrowser.Aggregate(({browser, total})=>{
         return {groupBy: {browser}, columns: {sum: {total}}}
     }).Sort(25, "total")
 
-    let signInCountry1 = fetchAzureSignInByCountryOrRegion("-30d<d","-25d>d")
-    let signInCountry2 = fetchAzureSignInByCountryOrRegion("-25d<d","-20d>d")
-    let signInCountry3 = fetchAzureSignInByCountryOrRegion("-20d<d","-15d>d")
-    let signInCountry4 = fetchAzureSignInByCountryOrRegion("-15d<d","-10d>d")
-    let signInCountry5 = fetchAzureSignInByCountryOrRegion("-10d<d","-5d>d")
-    let signInCountry6 = fetchAzureSignInByCountryOrRegion("-5d<d","-1d>d")
-    let signInCountry = mergeTable(signInCountry1, signInCountry2, signInCountry3, signInCountry4, signInCountry5, signInCountry6).Aggregate(({countryOrRegion, total})=>{
+    signInCountry = signInCountry.Aggregate(({countryOrRegion, total})=>{
         return {groupBy: {countryOrRegion}, columns: {sum: {total}}}
     }).Sort(25, "total")
 
-    let signInCity1 = fetchAzureSignInByCity("-30d<d","-25d>d")
-    let signInCity2 = fetchAzureSignInByCity("-25d<d","-20d>d")
-    let signInCity3 = fetchAzureSignInByCity("-20d<d","-15d>d")
-    let signInCity4 = fetchAzureSignInByCity("-15d<d","-10d>d")
-    let signInCity5 = fetchAzureSignInByCity("-10d<d","-5d>d")
-    let signInCity6 = fetchAzureSignInByCity("-5d<d","-1d>d")
-    let signInCity = mergeTable(signInCity1, signInCity2, signInCity3, signInCity4, signInCity5, signInCity6).Aggregate(({city, total})=>{
+    signInCity = signInCity.Aggregate(({city, total})=>{
         return {groupBy: {city}, columns: {sum: {total}}}
     }).Sort(15, "total")
 
-    let userEmailFreq1 = fetchAzureSignInByUserPrincipalName("-30d<d","-25d>d")
-    let userEmailFreq2 = fetchAzureSignInByUserPrincipalName("-25d<d","-20d>d")
-    let userEmailFreq3 = fetchAzureSignInByUserPrincipalName("-20d<d","-15d>d")
-    let userEmailFreq4 = fetchAzureSignInByUserPrincipalName("-15d<d","-10d>d")
-    let userEmailFreq5 = fetchAzureSignInByUserPrincipalName("-10d<d","-5d>d")
-    let userEmailFreq6 = fetchAzureSignInByUserPrincipalName("-5d<d","-1d>d")
-    let userEmailFreq = mergeTable(userEmailFreq1, userEmailFreq2, userEmailFreq3, userEmailFreq4, userEmailFreq5, userEmailFreq6).Aggregate(({userPrincipalName, total})=>{
+    userEmailFreq = userEmailFreq.Aggregate(({userPrincipalName, total})=>{
         return {groupBy: {userPrincipalName}, columns: {sum: {total}}}
     })
 
