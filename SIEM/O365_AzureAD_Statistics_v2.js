28a1d2ac307640925394fa0a95723b059cac27ea
Yuan Shen Tay <yuanshentay@gmail.com>
2023-09-20 11:28:48 -0400

    improved O365 azuread stats report

--- SIEM/O365_AzureAD_Statistics_v2.js
+++ SIEM/O365_AzureAD_Statistics_v2.js
@@ -1,48 +1,34 @@
 // Description:
 // Various statistics from Office365 AzureActiveDirectory audit data
+function getOfficeAADStats(from, to, group) {
+    let env = {from, to, group}
+    if (group == "country") {
+        let fplTemplate = `
+            search {from="{{.from}}", to="{{.to}}"} sContent("@sender","office365") and sContent("@source","Audit.AzureActiveDirectory")
+            let {{.group}}  = f("@fields._ip.{{.group}}")
+            aggregate events = count() by {{.group}}
+            sort 15 events
+        `
+        let table = fluencyLavadbFpl(template(fplTemplate, env))
+        return table
+    } else {
+        let fplTemplate = `
+        search {from="{{.from}}", to="{{.to}}"} sContent("@sender","office365") and sContent("@source","Audit.AzureActiveDirectory")
+        let {{.group}}  = f("@fields.{{.group}}")
+        aggregate events = count() by {{.group}}
+        sort 15 events
+        `
+        let table = fluencyLavadbFpl(template(fplTemplate, env))
+        return table
+    }
+}
 
 function main() {
-    let office_aad_by_ops_fpl = `
-        search {from="-24h@h", to="@h"} sContent("@sender","office365") and sContent("@source","Audit.AzureActiveDirectory")
-        let {Operation} = f("@fields")
-        aggregate events = count() by Operation
-        sort 15 events
-    `
-    
-    let office_aad_by_userid_fpl = `
-        search {from="-24h@h", to="@h"} sContent("@sender","office365") and sContent("@source","Audit.AzureActiveDirectory")
-        let {UserId} = f("@fields")
-        aggregate events = count() by UserId
-        sort 15 events
-    `
-    
-    let office_aad_by_application_fpl = `
-        search {from="-24h@h", to="@h"} sContent("@sender","office365") and sContent("@source","Audit.AzureActiveDirectory")
-        let {ApplicationName} = f("@fields")
-        aggregate events = count() by ApplicationName
-        sort 15 events
-    `
-    
-    let office_aad_by_ip_fpl = `
-        search {from="-24h@h", to="@h"} sContent("@sender","office365") and sContent("@source","Audit.AzureActiveDirectory")
-        let {ClientIP} = f("@fields")
-        aggregate events = count() by ClientIP
-        sort 15 events
-    `
-
-    let office_aad_by_country_fpl = `
-        search {from="-24h@h", to="@h"} sContent("@sender","office365") and sContent("@source","Audit.AzureActiveDirectory")
-        let {country} = f("@fields._ip")
-        aggregate events = count() by country
-        sort 15 events
-    `
-
-    let office_aad_by_ops = fluencyLavadbFpl(office_aad_by_ops_fpl)
-    let office_aad_by_userid = fluencyLavadbFpl(office_aad_by_userid_fpl)
-    let office_aad_by_application = fluencyLavadbFpl(office_aad_by_application_fpl)
-    let office_aad_by_ip = fluencyLavadbFpl(office_aad_by_ip_fpl)
-    let office_aad_by_country = fluencyLavadbFpl(office_aad_by_country_fpl)
+    let office_aad_by_ops = getOfficeAADStats("-24h@h", "@h", "Operation")
+    let office_aad_by_userid = getOfficeAADStats("-24h@h", "@h", "UserId")
+    let office_aad_by_application = getOfficeAADStats("-24h@h", "@h", "ApplicationName")
+    let office_aad_by_ip = getOfficeAADStats("-24h@h", "@h", "ClientIP")
+    let office_aad_by_country = getOfficeAADStats("-24h@h", "@h", "country")
     
     return {office_aad_by_ops, office_aad_by_userid, office_aad_by_application, office_aad_by_ip, office_aad_by_country}
 }
-
