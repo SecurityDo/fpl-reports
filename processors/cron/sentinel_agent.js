8e3517fa599fb71891af0d820768eb6c6d062c13
Kun@Stream <kun@fluencysecurity.com>
2024-07-05 15:10:09 -0400

    add sentinelone agent loading script

---
+++ processors/cron/sentinel_agent.js
@@ -0,0 +1,33 @@
+function main() {
+    let table = loadSentinelOneAgents()
+    let list = table.Map( (row) => row)
+    Platform_Asset_Refresh("SentinelOne", list)
+    return {table}
+}
+
+function loadSentinelOneAgents() {
+  let table =  Fluency_ResourceLoad("SentinelOne", "agent", "*", (obj, customer) => {
+    let fields= obj["@sentinelOneAgent"]
+    let {updatedAt, computerName:name, uuid:agentID, modelName:model, osName:os, machineType, lastIpToMgmt:privateIP, externalIp: publicIP} = fields
+    let timestamp = obj["@timestamp"]
+    return {
+      aggregate: {
+        groupBy: {agentID},
+        columns: {
+          argmax: {
+            updatedAt,
+            name,
+            model,
+            privateIP,
+            publicIP,
+            os,
+            machineType,
+            customer,
+            timestamp
+          }
+        }
+      }
+    }
+  })
+  return table
+}
