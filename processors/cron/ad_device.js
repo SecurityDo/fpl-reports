3ea6c6a1f41ba8ebbd41c6edcc06c2d50731ad7a
Kun@Stream <kun@fluencysecurity.com>
2024-07-05 15:29:03 -0400

    add AD load asset

---
+++ processors/cron/ad_device.js
@@ -0,0 +1,28 @@
+
+function main() {
+    let table = loadADAssets()
+    let list = table.Map( (row) => row)
+    Platform_Asset_Refresh("AD", list)
+    return {table}
+}
+
+
+function loadADAssets() {
+
+  let assets = Fluency_ResourceLoad("AD", "asset", "*", (obj, customer) => {
+      let fields = obj["@ADAsset"]
+      let {customer, name, description, dNSHostName, updatedOn, propertyFlags} = fields
+      if (propertyFlags.Some((_, e) => e == "ACCOUNTDISABLE")) {
+        return null
+      }
+      return {
+         aggregate:{
+           groupBy:{name},
+           columns: {
+             argmax:{updatedOn, description, dNSHostName, flags:propertyFlags, customer}
+           }
+         }
+      }
+  })
+  return {assets}
+}
